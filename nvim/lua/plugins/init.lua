dd49a0424605d557c6c2d0ed64e143bcd60310b0
denstiny <2254228017@qq.com>
2025-12-11 00:18:38 +0800

    feat: 删除with-key

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,28 +1,27 @@
 return {
-  "folke/neodev.nvim",
-  "folke/which-key.nvim",
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
+	"folke/neodev.nvim",
+	{ "folke/neoconf.nvim", cmd = "Neoconf" },
 
- {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "vim",
-        "lua",
-        "vimdoc",
-        "html",
-        "css",
-      },
-    },
-  },
-  {
-    "glepnir/lspsaga.nvim",
-  },
-  {
-    "mbbill/undotree",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"vim",
+				"lua",
+				"vimdoc",
+				"html",
+				"css",
+			},
+		},
+	},
+	{
+		"glepnir/lspsaga.nvim",
+	},
+	{
+		"mbbill/undotree",
 		config = function()
 			vim.g.undotree_SetFocusWhenToggle = 1
 		end
-  },
+	},
 
 }
