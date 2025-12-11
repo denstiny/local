9a0af820e00adecb52f7ca7b3c593911d3cf871b
denstiny <2254228017@qq.com>
2025-12-11 13:17:53 +0800

    feat: add nerd font

---
+++ fonts/install.sh
@@ -0,0 +1,14 @@
+# @author      : aero (2254228017@qq.com)
+# @file        : install
+# @created     : 星期三 2月 09, 2022 12:51:21 CST 
+# @github      : https://github.com/denstiny
+# @blog        : https://denstiny.github.io
+
+#!/bin/bash
+# 下载字体
+#sudo pacman -S noto-fonts-cjk ttf-joypixels ttf-firacode-nerd
+mkdir -p ~/.local/share/fonts
+cp ./*.otf ~/.local/share/fonts/
+cp ./*.ttf ~/.local/share/fonts/
+# 设置字体
+#cp font.conf /etc/fonts/conf.d/fonts.conf
