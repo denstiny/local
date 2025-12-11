b77b3c63e6e9b557e3acaa91803e283aaad4255d
denstiny <2254228017@qq.com>
2025-12-11 13:28:49 +0800

    feat: 添加部署脚本

---
+++ install_mac.sh
@@ -0,0 +1,22 @@
+# mac install local config script
+
+brew install python
+brew install golang
+brew install neovim
+brew install zshrc
+brew install npm
+brew install yazi
+
+CURRENT_DIR=$(cd "$(dirname "${CURRENT_FILE}")" && pwd -P)
+# [zsh]
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
+brew install autojump
+brew install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions 
+ln -s $CURRENT_DIR/.zshrc ~/.zshrc
+source ~/.zshrc 
+
+#[config]
+ln -s $CURRENT_DIR/yazi ~/.config/yazi
+ln -s $CURRENT_DIR/wezterm ~/.config/wezterm
+ln -s $CURRENT_DIR/nvim ~/.config/nvim
