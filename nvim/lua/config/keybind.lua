70de0d8bba59279e5784909a4fb98035f1231f47
denstiny <2254228017@qq.com>
2025-12-12 22:43:07 +0800

    feat: 创建tmux配置

--- nvim/lua/config/keybind.lua
+++ nvim/lua/config/keybind.lua
@@ -3,6 +3,6 @@ local del = vim.keymap.del
 map("n", "tr", "<cmd>NvimTreeToggle<cr>", { desc = "nvimtree toggle window" })
 map("n", "U", "<cmd>UndotreeToggle<cr>", { desc = "undo tree" })
 map("n", "L", "<cmd>Telescope oldfiles<cr>", { desc = "old files list" })
-map("n", "<c-b>", "<cmd>Telescope buffers<cr>", { desc = "buffers" })
+map("n", "B", "<cmd>Telescope buffers<cr>", { desc = "buffers" })
 map("n", "<c-f>", "<cmd>Telescope fd<cr>", { desc = "file list" })
 map("n", "fs", "<cmd>Telescope grep_string<cr>", { desc = "find string" })
