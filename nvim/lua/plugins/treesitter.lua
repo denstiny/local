3e357a8782eafb6729cc0d5834622363ee99964e
denstiny <2254228017@qq.com>
2025-12-13 17:50:20 +0800

    feat: treesitter switch branch main

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,13 +2,27 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	event = { "BufReadPost", "BufNewFile" },
 	cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+	branch = "main",
+	lazy = false,
 	build = ":TSUpdate",
-	opts = {
-		ensure_installed = { "lua", "luadoc", "printf", "vim", "vimdoc" },
-		highlight = {
-			enable = true,
-			use_languagetree = true,
-		},
-		indent = { enable = true },
-	},
+	config = function()
+		local opts = {
+			indent = { enable = true },
+			highlight = { enable = true, use_languagetree = true },
+			folds = { enable = true },
+			install = { "lua", "luadoc", "vim", "vimdoc", "go", "python", "rust" },
+		}
+		require("nvim-treesitter").setup(opts)
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = opts.install,
+			callback = function(args)
+				pcall(function()
+					vim.treesitter.start()
+					vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+					vim.wo.foldmethod = "expr"
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end)
+			end,
+		})
+	end,
 }
